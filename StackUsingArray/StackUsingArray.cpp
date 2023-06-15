3c30e8182fd404c6d9d28ecd6451f6d45051fc40
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2023-06-15 10:34:22 +0700

    membuat program utama

--- StackUsingArray/StackUsingArray.cpp
+++ StackUsingArray/StackUsingArray.cpp
@@ -3,23 +3,3 @@
 
 using namespace std;
 
-class Program {
-private:
-	string stack_array[5];
-	int top;
-
-public:
-	Program() {
-		top = -1;
-	}
-
-	string push(string element) {
-		if (top == 4) {
-			cout << "stack Full" << endl;
-			return "";
-		}
-
-		top++;
-		stack_array[top] = element;
-	}
-};
